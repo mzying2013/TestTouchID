e2a1ee99ddbb2264308713d1f89ab3b05c85adf3
Bill liu <317590898@qq.com>
2014-11-05 10:58:27 +0800

    iOS8 TouchID Demo

--- TestTouchID/ViewController.m
+++ TestTouchID/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <LocalAuthentication/LocalAuthentication.h>
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *touchIDResult;
+@property (nonatomic, strong) LAContext *context;
+- (IBAction)touchIDAction:(id)sender;
 
 @end
 
@@ -17,6 +21,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +29,63 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+
+
+
+//TouchID是否验证成功
+-(void)touchIDSuccess{
+    __block NSString *msg;
+    
+    [self.context evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics
+                 localizedReason:NSLocalizedString(@"解锁 合时代金融", nil)
+                           reply:^(BOOL success, NSError *error) {
+                               if (success) {
+                                   msg = [NSString stringWithFormat:NSLocalizedString(@"EVALUATE_POLICY_SUCCESS", nil)];
+                               }else{
+                                   msg = [NSString stringWithFormat:NSLocalizedString(@"EVALUATE_POLICY_WITH_ERROR", nil), error.localizedDescription];
+                               }
+                               NSLog(@"msg:%@",msg);
+                               dispatch_async(dispatch_get_main_queue(), ^{
+                                   self.touchIDResult.text = msg;
+                               });
+    }];
+    
+    
+}
+
+
+
+
+//TouchID是否有效
+-(BOOL)isValidateTouchID{
+    __block NSString *message;
+    NSError *error;
+    BOOL success;
+    
+    success = [self.context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics
+                                        error:&error];
+    
+    if (success) {
+        message = [NSString stringWithFormat:NSLocalizedString(@"TOUCH_ID_IS_AVAILABLE", nil)];
+    }else{
+        message = [NSString stringWithFormat:NSLocalizedString(@"TOUCH_ID_IS_NOT_AVAILABLE", nil)];
+    }
+    
+    NSLog(@"message:%@",message);
+    return success;
+}
+
+
+
+- (IBAction)touchIDAction:(id)sender {
+    if ([[UIDevice currentDevice].systemVersion floatValue] >= 8) {
+        self.context = [[LAContext alloc] init];
+        self.touchIDResult.text = @"指纹识别";
+        
+        if ([self isValidateTouchID]) {
+            [self touchIDSuccess];
+        }
+    }
+}
 @end
